10d9d216708f0d55f9084d1a711f087e601d6100
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2024-11-21 08:53:08 -0300

    chore: adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,6 +106,11 @@ export default function RootLayout({
           name="google-adsense-account"
           content="ca-pub-6396092891104748"
         ></meta>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6396092891104748"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen justify-center items-center bg-zinc-100 px-4 py-2 dark:bg-zinc-900`}
